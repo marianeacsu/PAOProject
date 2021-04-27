2f4432b05759a7fcadd4469a97b45b38e8291fd2
marianeacsu <58784210+marianeacsu@users.noreply.github.com>
2021-04-28 00:02:48 +0300

    etapa 2

---
+++ src/ReadFromCSV.java
@@ -0,0 +1,144 @@
+import People.Client;
+import Restaurant.*;
+import Others.*;
+import Order.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class ReadFromCSV {
+
+    public static void CSVDriver(){
+
+        //CSV = Comma-Separated Values
+        //   text file that uses a comma to separate values
+
+        String file = "src\\People\\driver.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+
+                String[] row = line.split(",");
+
+                for (String index : row) {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void CSVClient(){
+
+        //CSV = Comma-Separated Values
+        //   text file that uses a comma to separate values
+
+        String file = "src\\People\\client.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+
+                String[] row = line.split(",");
+
+                for (String index : row) {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void CSVProduct(){
+
+        //CSV = Comma-Separated Values
+        //   text file that uses a comma to separate values
+
+        String file = "src\\Restaurant\\product.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+
+                String[] row = line.split(",");
+
+                for (String index : row) {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void CSVLocation(){
+
+        //CSV = Comma-Separated Values
+        //   text file that uses a comma to separate values
+
+        String file = "src\\Others\\location.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+
+                String[] row = line.split(",");
+
+                for (String index : row) {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
